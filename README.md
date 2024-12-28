1b70c7971a0969bb0486cad49d1bef4264f0a70a
Ronald Leung <rcmleung@gmail.com>
2024-12-27 23:18:51 -0800

    change to cli args

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Simple Hugging face model downloader to local
+## Downloads specified HF model (ex. sentence-transformers/all-MiniLM-L6-v2) under a specified directory.
 
 ### Set up environment:
 ```bash
@@ -9,5 +10,10 @@ hf_model_downloader % pip install -U sentence-transformers
 
 ### Usage:
 ```bash
-python main.py
+python main.py --model <hf_model> --dir <local_dir>
+```
+
+### Example
+```bash
+python main.py --model sentence-transformers/all-MiniLM-L6-v2 --dir /local_dir
 ```
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from transformers import AutoTokenizer, AutoModel
+import argparse
 
 def main():
-    
-    hugging_face_model_name = input("What is the Hugging face model you want to download? ")
+    parser = argparse.ArgumentParser(description="Hugging face model downloader")
+    parser.add_argument('--model', type=str, help="Hugging face model (ex. sentence-transformers/all-MiniLM-L6-v2)")
+    parser.add_argument('--dir', type=str, help="Local download directory (ex. /local_dir)")
+    args = parser.parse_args()
     
     print("Fetching...")
+    hugging_face_model_name  = args.model
     tokenizer = AutoTokenizer.from_pretrained(hugging_face_model_name)
     model = AutoModel.from_pretrained(hugging_face_model_name)
     
-    local_directory = "/local_models/" + hugging_face_model_name
+    local_directory = args.dir + "/" + hugging_face_model_name
     print("Saving to " + local_directory + "...")
     tokenizer.save_pretrained(local_directory)
     model.save_pretrained(local_directory)
